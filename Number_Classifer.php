c2c348a194be8a65f78ebb2ccb73f310ef2d5a82
Fahimfaisaltalha <faisal15-4721@diu.edu.bd>
2025-03-05 00:01:04 +0600

    Nnumber Classifer

--- Number_Classifer.php
+++ Number_Classifer.php
@@ -1,1 +1,13 @@
 <?php
+
+echo "Enter the Number You want to classify :\n";
+$num=(int)readline();
+if($num>1){
+    echo "The number you entered is positive.";  
+}
+elseif($num<0){
+    echo "The number you entered is negative.";
+}
+else{
+    echo "The number you entered is zero.";
+}
